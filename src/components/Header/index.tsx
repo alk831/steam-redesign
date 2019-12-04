334500a86f24abaebc72569cf6eada6ce6359846
alk831 <adam54111@gmail.com>
2019-12-04 13:22:43 +0100

    style navbar and side menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { HEADER_LINKS } from '../../consts';
+import css from './index.module.scss';
 
 export const Header = () => {
   return (
-    <header>
+    <header className={css.container}>
       <nav>
-        <ul>
+        <ul className={css.links_container}>
           {HEADER_LINKS.map(link => (
             <li key={link.title}>
-              <a href={link.path} title={link.title}>
+              <a className={css.link} href={link.path} title={link.title}>
                 {link.title}
               </a>
             </li>
           ))}
         </ul>
       </nav>
-      <div>
-        <button>Login</button>
-        <button>Install Steam</button>
+      <div className={css.action_buttons_container}>
+        <a className={css.action_button}>
+          Login
+        </a>
+        <a className={css.action_button}>
+          Install Steam
+        </a>
       </div>
     </header>
   );
--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { CATEGORY_LINKS } from '../../consts';
+import css from './index.module.scss';
 
 export const SideMenu = () => {
   return (
     <nav>
-      {CATEGORY_LINKS.map((([title, links]) => (
-        <li key={title}>
-          {title}
-          <ul>
-            {(links as any).map((link: any) => (
-              <li key={link}>
-                <a href="/" title={link}>
-                  {link}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </li>
-      )))}
+      <ul className={css.categories_container}>
+        {CATEGORY_LINKS.map((([title, links]) => (
+          <li key={title} className={css.category_container}>
+            {title}
+            <ul className={css.links_container}>
+              {(links as any).map((link: any) => (
+                <li key={link}>
+                  <a
+                    href="/"
+                    title={link}
+                    className={css.category_link}
+                  >
+                    {link}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </li>
+        )))}
+      </ul>
     </nav>
   );
 }
